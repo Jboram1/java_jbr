7faf2451c4c1740defc791973d5a671f2923d401
KOREAVC <KOREAVC@DESKTOP-0R26I5N>
2023-08-31 18:15:05 +0900

    8월 31일 수업내용 변수, 연산자

---
+++ p0831/src/p0831/Jbr_01.java
@@ -0,0 +1,21 @@
+package p0831;
+
+public class Jbr_01 {
+
+	public static void main(String[] args) {
+		// 국어, 영어, 수학, 사회 평균을 출력 소수점 3자리까지 출력하시오.
+		//100,100,99,80
+		
+		
+		int kor =0;
+		int eng =0;
+		int math =0;
+		int total =kor+eng+math;
+		double avg =total/3.0;
+		
+		
+		
+
+	}
+
+}
